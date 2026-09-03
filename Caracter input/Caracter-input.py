8de5068d2e33f72e06a53685e2057feb0a5eeb0b
FahdJ0 <2rechange@gmail.com>
2026-09-03 09:05:15 +0100

    Premier commit : Character Input

---
+++ Caracter input/Caracter-input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age= int(input("Enter your age: "))
+hundred = 2026 - age + 100
+message = f"Hello {name}, you will be 100 years old in {hundred}"
+print(message)
+number = int(input("Enter a number: "))
+print((message + "\n") * number)
